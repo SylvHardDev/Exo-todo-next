7a0ff2b11119f3f6a0c55e4f956c84b92d3bcf71
SylvHardDev <hardsyldev@gmail.com>
2024-12-17 13:47:23 +0300

    add input page  new todo

---
+++ src/app/new/page.tsx
@@ -0,0 +1,13 @@
+export default function page() {
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">New</h1>
+      </header>
+
+      <form className="flex  gap-2 flex-col">
+        <input type="text" name="title" className="border border-slat-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"/>
+      </form>
+    </>
+  );
+}
