c65418b87359f0b976ee897f61865c9173f29b05
SylvHardDev <hardsyldev@gmail.com>
2024-12-17 11:49:27 +0300

    header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <header className="flex justify-between items-center">
+        <h1 className="text-2xl  mb-4">Todos</h1>
+        <Link className="border border-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href="/new">New</Link>
+      </header>
+    </>
+  );
 }
+ 
